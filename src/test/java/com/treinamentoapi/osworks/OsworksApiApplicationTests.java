2b6b5342520f0cb0ef3855ed7e4a13212abd475b
Pedro Augusto <peedroaugustto@hotmail.com>
2021-03-09 14:08:23 -0300

    criação do projeto

---
+++ src/test/java/com/treinamentoapi/osworks/OsworksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamentoapi.osworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsworksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
