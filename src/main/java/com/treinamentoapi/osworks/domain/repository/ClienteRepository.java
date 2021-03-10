27e544f8df8ea486a85973d607ac15e1a72a2a71
Pedro Augusto <peedroaugustto@hotmail.com>
2021-03-10 11:29:43 -0300

    Alterações Cliente Controller

---
+++ src/main/java/com/treinamentoapi/osworks/domain/repository/ClienteRepository.java
@@ -0,0 +1,15 @@
+package com.treinamentoapi.osworks.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.treinamentoapi.osworks.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	
+	List<Cliente> findByNomeContaining(String nome);
+
+}
