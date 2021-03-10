530f9f35ecdbd406646b1efd0e21dc9b645b4187
Pedro Augusto <peedroaugustto@hotmail.com>
2021-03-10 12:01:31 -0300

    Adicionado e configurado Bean Validation

--- src/main/java/com/treinamentoapi/osworks/api/controller/ClienteController.java
+++ src/main/java/com/treinamentoapi/osworks/api/controller/ClienteController.java
@@ -3,6 +3,8 @@ package com.treinamentoapi.osworks.api.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,14 +48,14 @@ public class ClienteController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar(@RequestBody Cliente cliente) {
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
 
 		return clienteRepository.save(cliente);
 
 	}
 
 	@PutMapping("{clienteId}")
-	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente) {
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,@Valid @RequestBody Cliente cliente) {
 
 		if (!clienteRepository.existsById(clienteId)) {
 			ResponseEntity.notFound().build();
